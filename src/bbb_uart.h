4ad9e32c21065eafc3e1296924a640f2a7e2ca26
Krygerdile <lnowakow@ucsd.edu>
2018-07-06 16:14:23 -0700

    Updated bbb_uart header and cpp, need to refine and test

--- src/bbb_uart.h
+++ src/bbb_uart.h
@@ -3,20 +3,29 @@
 
 #include <termios.h>
 
+enum UART_TYPE {TX = 0, RX = 1, BOTH = 2};
+
 class UART {
     private:
         int uartNum;
         int baudRate;
         int uartID;
+        UART_TYPE uartType;
         struct termios* uartTerm;
-
+        
 
     public:
-        UART(int uartNum, int baudRate);
+        UART(int uartNum, int baudRate, UART_TYPE uartType);
         
-        int write(char data);
+        int init();
+
+        int uart_write(unsigned char* data, int len);
         
+        int uart_read(void* buffer, int len);
+
         int status();
+        
+        int getID();
 
         ~UART();
 
