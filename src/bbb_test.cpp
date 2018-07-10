18807608c15863b236ce1f8819254cce789bacd0
Krygerdile <lnowakow@ucsd.edu>
2018-07-09 18:32:29 -0700

    Updated test file for BBB to see if class works

---
+++ src/bbb_test.cpp
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <string.h>
+
+#include "bbb_uart.h"
+
+int main() {
+        
+    UART test = UART(4, 250000, TX, true);
+    test.init();
+
+    const char* msg = "Hello this is a test";
+
+    while(1) {
+        if (test.uart_write((void*) msg, strlen(msg)) < 0) {
+            break;
+        }
+        
+        usleep(1000);
+    }
+
+    return 0;
+
+}
